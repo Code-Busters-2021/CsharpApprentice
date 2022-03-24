d3e8aeb5a82c367608246c79ace508d450177d55
a.gharsalli <a.gharsalli@criteo.com>
2022-03-24 16:27:58 +0100

    [IEnumerable] Add InvertedEnumerable

--- CollectionRewrite/Invert.cs
+++ CollectionRewrite/Invert.cs
@@ -1,18 +1,53 @@
-using CollectionRewrite;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 
-namespace TestCollection
+namespace CollectionRewrite
 {
-    public class ListShould
+    public class InvertedEnumerable : IEnumerable<double>
     {
-        public IEnumerable<float> Invert(IEnumerable<int> IntEnum) 
-        {
-            IEnumerable<float> listFloat = new IEnumerable<float>();
+        public static IEnumerable<double> InvertNumbers(IEnumerable<int> originalEnumerable) => new InvertedEnumerable(originalEnumerable);
 
-            foreach (int elem in IntEnum) {
-                listFloat.Add(1 / elem);
+        private readonly IEnumerable<int> _originalEnumerable;
+
+        public InvertedEnumerable(IEnumerable<int> originalEnumerable)
+        {
+            _originalEnumerable = originalEnumerable;
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            return new InvertedEnumerator(_originalEnumerable.GetEnumerator());
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class InvertedEnumerator : IEnumerator<double>
+        {
+            private readonly IEnumerator<int> _originalValues;
+
+            public InvertedEnumerator(IEnumerator<int> originalValues)
+            {
+                _originalValues = originalValues;
             }
-            return (listFloat as IEnumerable<float>);
+            public bool MoveNext() => _originalValues.MoveNext();
+
+            public void Reset()
+            {
+                _originalValues.Reset();
+            }
+
+            public double Current => 1d / _originalValues.Current ;
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+                Reset();
+            }
         }
     }
 }
