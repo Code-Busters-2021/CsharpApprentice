e14ea17053c864d13f7a05d29552db282bea9653
AyouBusters <101646362+AyouBusters@users.noreply.github.com>
2022-04-05 17:45:52 +0200

    Add Chou skeleton (#2)
    Co-authored-by: a.gharsalli <a.gharsalli@criteo.com>

--- RepairMan/Program.cs
+++ RepairMan/Program.cs
@@ -1,53 +1,84 @@
 using System;
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var wm = new WashingMachine();
-Machine m = new WashingMachine();
-CoffeeMachine cm = new CoffeeMachine();
-RepairMan.Repair(cm);
-RepairMan.Repair(wm);
-RepairMan.Repair(m);
+using System.Diagnostics;
 
 
-public interface Machine
+namespace Prog
 {
-    public void Work();
-}
+    class Program
+    {
 
-public class CoffeeMachine : Machine
-{
-    public void Work()
-    {
-        MakeCoffee();
+        static Machine CreateMachine()
+        {
+            if (DateTime.Now.Hour < 8)
+            {
+                return new CoffeeMachine();
+            }
+
+            return new WashingMachine();
+
+        }
+
+        public static void Main(string[] args)
+        {
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+
+            RepairMan.Repair(CreateMachine());
+        }
     }
 
-    private void MakeCoffee()
+    public interface Machine
     {
-        System.Console.WriteLine("I'm a coffee machine and I'm making coffee");
+        public void Work();
     }
-}
 
-public class WashingMachine : Machine
-{
-    public void Work()
+    public class CoffeeMachine : Machine
     {
-        System.Console.WriteLine("I'm a washing machine and I'm washing");
-    }
-}
+        public void Work()
+        {
+            MakeCoffee();
+        }
 
-public class RepairMan
-{
-    public static void Repair(Machine m)
-    {
-        throw new Exception("Unknown machine type");//todo change this to repair the good machine
+        private void MakeCoffee()
+        {
+            System.Console.WriteLine("I'm a coffee machine and I'm making coffee");
+        }
     }
-    public static void Repair(WashingMachine m)
+
+    public class WashingMachine : Machine
     {
-        System.Console.WriteLine("I'm repairing a washing machine");
+        public void Work()
+        {
+            System.Console.WriteLine("I'm a washing machine and I'm washing");
+        }
     }
-    public static void Repair(CoffeeMachine m)
+
+    public class RepairMan
     {
-        System.Console.WriteLine("I'm repairing a coffee machine");
+        public static void Repair(Machine m)
+        {
+            var resul = m switch
+            {
+                CoffeeMachine cm => Repair(cm),
+                WashingMachine wm => Repair(wm),
+                _ => throw new Exception($"Unknown Machine type {m.GetType()}")
+            };
+
+          
+        }
+
+        public static bool Repair(WashingMachine m)
+        {
+            System.Console.WriteLine("I'm repairing a washing machine");
+            return true;
+        }
+
+        public static bool Repair(CoffeeMachine m)
+
+        {
+            Console.WriteLine("I'm repairing a coffee machine");
+            return true;
+        }
     }
 }
